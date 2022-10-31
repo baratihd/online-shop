2d26bbcc0b88ac86a619bd3ff64847b105ee9221
baratihd <barati1989@gmail.com>
2022-10-31 15:04:42 +0330

    create category model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from .constants import models_verbose_names
+
+
+class CategoryModel(models.Model):
+    parent = models.ForeignKey(
+        'self',
+        related_name='children',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name=models_verbose_names.PARENT
+    )
+    title = models.CharField(
+        max_length=70,
+        verbose_name=models_verbose_names.TITLE
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=models_verbose_names.CREATED_AT
+    )
