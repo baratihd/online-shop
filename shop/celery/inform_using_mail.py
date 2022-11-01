47b39da2aef2ee011d497f0e0774bac5067c796c
baratihd <barati1989@gmail.com>
2022-11-01 18:54:47 +0330

    add celery to project

---
+++ shop/celery/inform_using_mail.py
@@ -0,0 +1,6 @@
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def send_mail_to(subject, message, receivers):
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [receivers], fail_silently= False)
